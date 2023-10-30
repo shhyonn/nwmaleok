6908af18b9945d5af31612fa2fb9981f201924b9
Yeva Rapovych <yrapovych@gmail.com>
2023-10-30 11:50:46 +0100

    Created folder c++  and files

---
+++ c++/if.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	short a, b; 
+	cout <<"podaj a";
+	cin>> a;
+	cout <<"podaj b";
+	cin >> b;
+	if(a>b){
+		cout <<"a>b";
+	
+	}
+	else{
+		cout <<"a<b";
+	}
+	
+}
