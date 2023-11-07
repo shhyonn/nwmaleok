872bfe0b37644e9aa71b85600742030496046c88
Yeva Rapovych (Uczeń) <yrapovych581@technikumkreatywne.pl>
2023-11-07 11:05:53 +0100

    Added file FOR

---
+++ c++/Untitled1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=0; i < 10; i++){
+        for(int j=0; j < 10; j++){
+            cout << i<< " * "<<j<<"="<<j*i<<endl;
+        }
+        cout<< endl;
+    }
+cout << endl;
+system;"pause";
+}
