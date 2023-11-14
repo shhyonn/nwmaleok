ba7bbaa5ea0e23e28b1892a49605682d088c9af7
Yeva Rapovych (Uczeń) <yrapovych581@technikumkreatywne.pl>
2023-11-14 10:42:44 +0100

    zmeniony plik KWADRAT

--- c++/kwadrat.cpp
+++ c++/kwadrat.cpp
@@ -6,20 +6,24 @@ int main(){
     int b;
     std::cout << "Podaj szyrokosc: ";
     std::cin >> a;
-    std::cout << "Podaj : ";
+    std::cout << "Podaj wysokosc : ";
     std::cin >>b;
 
    for(int i=0; i < b; i++){
        for(int j=0; j < a; j++){
+       	if (i==0 || i == b-1)
            cout << "*";
-           if (i<b;j<a)for(int i=0; i < b-1; i++){
-       			for(int j=0; j < a-1; j++){
-           cout << " ";;
+        else if(j==0 || j == a-1)
+        	cout << "*";
+        else 
+        cout << " ";
+           
+           
+         
            
        }
+       cout << endl;  
        }
  
-       cout << endl;
+       
    }
-}
-}
