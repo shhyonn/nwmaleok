0547d1dd3485a367af79fc7520027fbe518a24e1
Yeva Rapovych (Uczeń) <yrapovych581@technikumkreatywne.pl>
2023-11-14 11:06:05 +0100

    zmeniony plik KWADRAT

---
+++ c++/dywan.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+    int b;
+    std::cout << "Podaj szyrokosc: ";
+    std::cin >> a;
+    std::cout << "Podaj wysokosc : ";
+    std::cin >>b;
+
+   for(int i=0; i < b; i++){
+       for(int j=0; j < a; j++){
+       	if (i==0 || i == b-1)
+           cout << "--";
+        else if(j==0 || j == a-1)
+        	cout << " | ";
+        else 
+        	if ()
+        	cout << "*#";
+           
+       }
+       cout << endl; 
+	   cout << "#*";
+	   cout << endl; 
+       }
+ 
+       
+   }
