998a6eb1726873ae37d787eab38c84289ac99104
Yeva Rapovych <yrapovych@gmail.com>
2023-12-04 11:23:27 +0100

    kartkówka

---
+++ c++/YevaRapovychKartkówka.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main() {
+	int a;
+	for (int a=12; a<= 40; a++ 1){ 
+		
+	
+		cout<<"Liczby od 12 do 40:";
+		cout<<a;
+		
+		if(a%%4==0 ){
+			
+			cout<< "Liczby podzielne przez 4(od 12 do 40):";
+			cout<<a;
+			
+		}
+			
+		
+		 
+	}
+	return 0;
+}
