535b4ba69d95b4153a3d38e537e25f2538f3f247
Yeva Rapovych (Uczeń) <yrapovych581@technikumkreatywne.pl>
2024-01-02 11:04:56 +0100

    funkcja1

--- c++/funkcja1.cpp
+++ c++/funkcja1.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
-
+void Przywitanie(int imie, int nazwisko);
 int main()
 {   
 
     string imie, nazwisko;
-    void Przywitanie(int imie, int nazwisko);
+    
     cout << "Podaj swoje imie: ";
     cin >> imie;
     cout << "Podaj swoje nazwisko: ";
     cin >> nazwisko;
-    Przywitanie(imie,nazwisko);
-    return Przywitanie;
-}
+   
+     cout <<Przywitanie;
+     return Przywitanie;
+} 
+Przywitanie(imie, nazwisko);
