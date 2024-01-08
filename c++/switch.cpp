e31694baa946a7f981ddcd2886a1e0673f78f449
Yeva Rapovych <yrapovych@gmail.com>
2024-01-08 12:00:15 +0100

    switch1

---
+++ c++/switch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int liczba1;
+	int liczba2;
+    cout << "Podaj1 liczbe: ";
+    cout << "Podaj2 liczbe: ";
+    cout << "Podaj operatora z (+,-,*,/)"
+    cin >> liczba1;
+    cin>> liczba2;
+    
+    cin>>operator;
+    switch( operator )
+    {
+    	case +:
+    		
+    	case -:
+    		
+    	case *:
+    		
+    	case /:
+    		
+    	
+	}
+   	
