ac07f0161a1439b9496db29a7d5821f051882e41
Yeva Rapovych (Uczeń) <yrapovych581@technikumkreatywne.pl>
2024-04-29 12:54:31 +0200

    rozkład liczby na czynniki pierwsze

---
+++ c++/rozkład liczb.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void rozklad(int n) {
+    for (int k = 2; n > 1; ++k) {
+        while (n % k == 0) {
+            cout << k << " ";
+            n /= k;
+        }
+    }
+    cout << endl;
+}
+ 
+int main() {
+    int n;
+    cout << "Podaj liczbe : ";
+    cin >> n;
+    cout << "Czynniki pierwsze liczby "<<n<<": ";
+    rozklad(n);
+    return 0;
+}
